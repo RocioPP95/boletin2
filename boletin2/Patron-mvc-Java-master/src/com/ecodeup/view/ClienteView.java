45a96301cfc46bca3db260a0e7a9c0a0e7f7cf43
Rocio Pacheco Ponce <rociopa1995@gmail.com>
2024-11-03 12:16:35 +0100

    boletin2

--- boletin2/Patron-mvc-Java-master/src/com/ecodeup/view/ClienteView.java
+++ boletin2/Patron-mvc-Java-master/src/com/ecodeup/view/ClienteView.java
@@ -2,7 +2,6 @@ package com.ecodeup.view;
 
 public class ClienteView {
 	public void imprimirDatosCliente(int id,String nombre, String apellido) {
-		System.out.println("**** DATOS CLIENTE ****");
 		System.out.println("Id: "+id);
 		System.out.println("Nombre: "+nombre);
 		System.out.println("Apellido: "+apellido);
